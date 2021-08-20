917188a5871de1cde63c5163977a18a2368038b8
David Son <61252774+davidson132@users.noreply.github.com>
2021-08-20 15:02:25 -0400

    Add files via upload

---
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AuthSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Areas.Identity.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<OpportunityList> OpportunityList { get; set; }
+
+    }
+}
