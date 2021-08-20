f0e8932a06942b03cb3414d2a84f6cbc7dcdc4e7
David Son <61252774+davidson132@users.noreply.github.com>
2021-08-20 15:03:23 -0400

    Add files via upload

---
+++ Models/OpportunityList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Models
+{
+    public class OpportunityList
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Opportunity Center")]
+        public string OpportunityCenter { get; set; }
+
+        [Required]
+        [DisplayName("Opportunity Title")]
+        public string OpportunityTitle { get; set; }
+    }
+}
