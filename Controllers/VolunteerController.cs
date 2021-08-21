339b885cedee427dae47c8eba509d3fc5219e2c8
David Son <61252774+davidson132@users.noreply.github.com>
2021-08-21 19:56:39 -0400

    Update VolunteerController.cs

--- Controllers/VolunteerController.cs
+++ Controllers/VolunteerController.cs
@@ -9,12 +9,101 @@ namespace volunteer_app.Controllers
 {
     public class VolunteerController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        
+        public VolunteerController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public IActionResult Index()
+        {
+            IEnumerable<VolunteerList> VolunteerList = _db.VolunteerList;
+            return View(VolunteerList);
+        }
+
+        //This method leads you to the page that allows you to add new volunteers
+        public IActionResult AddVolunteer()
         {
             return View();
         }
 
+        //This method will actually add the new volunteer to the database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddVolunteer(VolunteerList vol)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.VolunteerList.Add(vol);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vol);
+        }
+
+        //This method leads you to the page that allows you to edit the volunteers
+        public IActionResult EditVolunteer(int? Id)
+        {
+
+            if (Id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.VolunteerList.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        //This method will update the edit made to the volunteer
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditVolunteer(VolunteerList vol)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.VolunteerList.Update(vol);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vol);
+        }
+
+        public IActionResult DeleteVolunteer(int? Id)
+        {
+
+            if (Id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.VolunteerList.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        //This method will update the database by deleting the entity
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteVolunteerEntity(int? Id)
+        {
+            var entity = _db.VolunteerList.Find(Id);
+            _db.VolunteerList.Remove(entity);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        
         // Logic to return view with data populated from the database
 
         //private readonly VolunteerModelContext _context;
