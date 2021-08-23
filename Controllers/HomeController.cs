24b929936205ba5da571c5e2fa9b8a35c78719ad
Tabitha <38332966+tabithasingh@users.noreply.github.com>
2021-08-22 21:51:34 -0400

    Update HomeController.cs
    Draft code for matching volunteers and opportunities

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using AuthSystem.Models;
+using AuthSystem.Areas.Identity.Data;
+using AuthSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -15,10 +16,12 @@ namespace AuthSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -32,11 +35,46 @@ namespace AuthSystem.Controllers
             return View();
         }
 
-       
+        public IActionResult VolunteerOpportunityMatches()
+        {
+
+            var volunteers = _db.VolunteerList.ToList();
 
 
+            var opportunities = _db.OpportunityList.ToList();
 
+            string[] vAvailability;
+            string[] oAvailability;
 
+            // Dictionary with volunteer as key, list of opportunities as value
+           //Dictionary<VolunteerList, List<OpportunityList>> vMatches = new Dictionary<VolunteerList, List<OpportunityList>>();
+           Dictionary<VolunteerList, List<string>> vMatches = new Dictionary<VolunteerList, List<string>>();
+            
+
+            foreach (var volunteer in volunteers) {
+                vAvailability = volunteer.Availability.Split(',');
+                //List<OpportunityList> oMatches = new List<OpportunityList>();
+                List<string> oMatches = new List<string>();
+                    foreach (var opportunity in opportunities)
+                {
+                    oAvailability = opportunity.Availability.Split(',');
+
+                    // Checks if there's any overlap between the volunteer and opportunity availability
+                    if (vAvailability.Where(oAvailability.Contains).Count() > 0)
+                    {
+                        oMatches.Add(opportunity.OpportunityTitle);
+                    }
+
+                }
+                vMatches.Add(volunteer, oMatches);
+            }
+
+            ViewBag.Keys = vMatches.Keys.ToList();
+            ViewBag.Values = vMatches.Values;
+
+            return View();
+
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
